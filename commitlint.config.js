e2a96d6b48e475eace1026ec760c5599736fcaee
srinivaskoruprolu007 <korruprolu-srinivasa.rao@capgemini.com>
2026-03-29 18:48:35 +0530

    chore: set up Husky hooks for commit message linting and pre-commit testing

---
+++ commitlint.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-empty": [2, "never"],
+		"type-case": [2, "always", "lower-case"],
+		"type-enum": [
+			2,
+			"always",
+			[
+				"build",
+				"chore",
+				"ci",
+				"docs",
+				"feat",
+				"fix",
+				"perf",
+				"refactor",
+				"revert",
+				"style",
+				"test",
+			],
+		],
+		"scope-empty": [2, "never"],
+		"scope-case": [2, "always", "lower-case"],
+		"subject-empty": [2, "never"],
+		"subject-full-stop": [2, "never", "."],
+		"subject-case": [
+			2,
+			"never",
+			["sentence-case", "start-case", "pascal-case", "upper-case"],
+		],
+		"header-max-length": [2, "always", 72],
+		"body-leading-blank": [1, "always"],
+		"footer-leading-blank": [1, "always"],
+	},
+};
